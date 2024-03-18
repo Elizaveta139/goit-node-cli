74154208ccf5dfc4ab6644b9e19e7d020fdf55a5
Elizaveta139 <elizavetarevina139@gmail.com>
2024-03-18 22:04:41 +0200

    дз

--- contacts.js
+++ contacts.js
@@ -2,34 +2,31 @@ const fs = require("fs/promises");
 const path = require("path");
 const { nanoid } = require("nanoid");
 
-const contactsPath = path.resolve("db", "contacts.json");
+const contactsPath = path.join(__dirname, "db", "contacts.json");
 
 async function listContacts() {
-  // ...твій код. Повертає масив контактів.
   const data = await fs.readFile(contactsPath);
   return JSON.parse(data);
 }
 
 async function getContactById(contactId) {
-  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
   const contacts = await listContacts();
-  const data = contacts.find((contact) => contact.id === contactId);
-  return data || null;
+  const result = contacts.find((contact) => contact.id === contactId);
+  return result || null;
 }
 
 async function removeContact(contactId) {
-  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
   const contacts = await listContacts();
   const index = contacts.findIndex((contact) => contact.id === contactId);
   if (index === -1) {
     return null;
   }
-  const data = contacts.splice(index, 1);
-  return data;
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
 }
 
-async function addContact(name, email, phone) {
-  // ...твій код. Повертає об'єкт доданого контакту (з id).
+async function addContact({ name, email, phone }) {
   const contacts = await listContacts();
   const newContact = {
     id: nanoid(),
@@ -37,11 +34,14 @@ async function addContact(name, email, phone) {
     email,
     phone,
   };
-
   contacts.push(newContact);
-
-  await fs.writeFile(contactsPath, JSON.stringify(contacts));
-  console.log(contacts);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
 }
 
-addContact("Ela", "Ela@mail.com", "19452656");
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-import { program } from "commander";
+const { program } = require("commander");
+const contacts = require("./contacts");
+
 program
   .option("-a, --action <type>", "choose action")
   .option("-i, --id <type>", "user id")
@@ -10,23 +12,26 @@ program.parse();
 
 const options = program.opts();
 
-// TODO: рефакторити
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      // ...
+      const allContacts = await contacts.listContacts();
+      console.table(allContacts);
       break;
 
     case "get":
-      // ... id
+      const oneContact = await contacts.getContactById(id);
+      console.table(oneContact);
       break;
 
     case "add":
-      // ... name email phone
+      const newContact = await contacts.addContact({ name, email, phone });
+      console.table(newContact);
       break;
 
     case "remove":
-      // ... id
+      const removeContact = await contacts.removeContact(id);
+      console.table(removeContact);
       break;
 
     default:
